7f0c8b1b9a1b10c7524a99030047aa27c13f0e63
huwenboshi <shihuwenbo@ucla.edu>
2017-11-22 15:36:54 -0800

    updated script to get snp chr bp a1 and a2

--- src/get_snp_chr_bp_a1_a2.py
+++ src/get_snp_chr_bp_a1_a2.py
@@ -11,7 +11,9 @@ for i in range(1,23):
     chrom_snp = []
     chrom_chr = []
     chrom_bp = []
-    
+    chrom_effect_allele = []
+    chrom_non_effect_allele = []
+
     legend_fnm = '{}/1000GP_Phase3_chr{}.legend.gz'.format(legend_dir, i)
     flr = False
     legend_f = gzip.open(legend_fnm, 'r')
@@ -22,15 +24,25 @@ for i in range(1,23):
         cols = line.strip().split()
         snp = cols[0].split(':')[0]
         if snp[0:2] != 'rs': continue
+        effect_allele = cols[3]
+        non_effect_allele = cols[2]
+        if len(effect_allele) != 1 or len(non_effect_allele) != 1:
+            continue
 
         chrom_snp.append(snp)
         chrom_chr.append(i)
         chrom_bp.append(cols[1])
-    
+        chrom_effect_allele.append(effect_allele)
+        chrom_non_effect_allele.append(non_effect_allele)
+
     legend_f.close()
 
-    all_chrom = pd.DataFrame({'SNP': chrom_snp, 'CHR': chrom_chr, 'BP': chrom_bp})
-    all_chrom = all_chrom[['SNP', 'CHR', 'BP']]
+    all_chrom = pd.DataFrame({'SNP': chrom_snp,
+                              'CHR': chrom_chr,
+                              'BP': chrom_bp,
+                              'A1': chrom_effect_allele,
+                              'A2': chrom_non_effect_allele})
+    all_chrom = all_chrom[['SNP', 'CHR', 'BP', 'A1', 'A2']]
     all_chrom = all_chrom.drop_duplicates('SNP', keep=False)
     all_chrom = all_chrom.drop_duplicates('BP', keep=False)
-    all_chrom.to_csv('1000G_SNP_CHR_BP.txt', '\t', mode='a', header=False, index=None)
+    all_chrom.to_csv('1000G_SNP_CHR_BP_A1_A2.txt', '\t', mode='a', header=False, index=None)
